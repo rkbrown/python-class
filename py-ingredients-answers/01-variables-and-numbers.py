5f07bd49aa9677fac06b1762d42888f77a7ff98c
Richard Brown <rbrown@rilm.org>
2017-03-24 16:29:30 -0400

    ingredients — vars and numbers

---
+++ py-ingredients-answers/01-variables-and-numbers.py
@@ -0,0 +1,81 @@
+print('***********************************************************************')
+print('************************ VARIABLES AND NUMBERS ************************')
+print('***********************************************************************')
+
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+# assign the number 25 to the variable names a and print it out
+a = 25
+print(a)
+
+print('')
+print('')
+print('(2) -------------------------------------------------------------------')
+print('')
+print('')
+# print out the number 2
+
+print(2)
+
+print('')
+print('')
+print('(3) -------------------------------------------------------------------')
+print('')
+print('')
+# print the sum of 1,299 and 3,056
+
+print(1299 + 3056)
+
+
+print('')
+print('')
+print('(4) -------------------------------------------------------------------')
+print('')
+print('')
+# print 963 subtracted from 27
+print(27 - 963)
+
+
+
+print('')
+print('')
+print('(5) -------------------------------------------------------------------')
+print('')
+print('')
+# multiply 387 times 5 (hint * means 'multplied by')
+
+print(387 * 5)
+
+print('')
+print('')
+print('(6) -------------------------------------------------------------------')
+print('')
+print('')
+# print 9 divided by 5 (hint / means 'divided by')
+
+print(9 / 5)
+
+
+
+print('')
+print('')
+print('(7) -------------------------------------------------------------------')
+print('')
+print('')
+# copy your division answer from above and change the / to //
+# what is the result? what does // do?
+
+print(9 // 5)
+
+
+print('')
+print('')
+print('(8) -------------------------------------------------------------------')
+print('')
+print('')
+# subtract 25,999 from the product of 189,992 and 2,769,232
+
+print(189992 * 2769232 - 25999)
