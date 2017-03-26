7c46bf8ff908965b9036ad39fb6c4e4af5fc7771
Richard Brown <rbrown@rilm.org>
2017-03-26 13:20:50 -0400

    misc

---
+++ py-ingredients/02-strings.py
@@ -0,0 +1,11 @@
+print('***********************************************************************')
+print('******************************** STRINGS ******************************')
+print('***********************************************************************')
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+# print out a string that says 'Hello, world!'
+# hint: you create a string by using quotes, single 'like this!' or double "like this!" 
+print("hello")
