c830c84602829cb1997519f7ee8fffc5dfe2a7dc
Richard Brown <rbrown@rilm.org>
2017-04-01 16:15:58 -0400

    start strings

--- py-ingredients/02-strings.py
+++ py-ingredients/02-strings.py
@@ -8,4 +8,80 @@ print('')
 print('')
 # print out a string that says 'Hello, world!'
 # hint: you create a string by using quotes, single 'like this!' or double "like this!" 
-print("hello")
+
+print('')
+print('')
+print('(2) -------------------------------------------------------------------')
+print('')
+print('')
+# create two variables called noun and verb, 
+# assign string values to each
+# combine the noun and verb variables and print out the combination in a sentence
+# for example, if your two string values are 'Belle' and 'run', a sentence could be: See Belle run. ('run' is not in a variable)
+# hint - you can combine string and variables with a plus sign (+)
+
+print('')
+print('')
+print('(3) -------------------------------------------------------------------')
+print('')
+print('')
+
+
+print('')
+print('')
+print('(3) -------------------------------------------------------------------')
+print('')
+print('')
+# create a variable call alphabet 
+# assign as its value the whole alphabet spelled out in a string (in order, without spaces)
+# print it out
+
+
+print('')
+print('')
+print('(4) -------------------------------------------------------------------')
+print('')
+print('')
+# print out the first letter of the alphabet
+
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+
+
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+
+
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+
+
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+
+
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+
+
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+
