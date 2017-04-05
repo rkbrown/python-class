f596f7a3942ce4a4ad9cdb62d1dcf81e6b9fd372
Richard Brown <rbrown@rilm.org>
2017-04-04 20:26:42 -0400

    strings

--- py-ingredients/02-strings.py
+++ py-ingredients/02-strings.py
@@ -9,32 +9,41 @@ print('')
 # print out a string that says 'Hello, world!'
 # hint: you create a string by using quotes, single 'like this!' or double "like this!" 
 
+
 print('')
 print('')
 print('(2) -------------------------------------------------------------------')
 print('')
 print('')
-# create two variables called noun and verb, 
+# create a variable named "words" and assign a string value to it, then print it out
+
+print('')
+print('')
+print('(3) -------------------------------------------------------------------')
+print('')
+print('')
+# create two variables called "noun" and "verb", 
 # assign string values to each
 # combine the noun and verb variables and print out the combination in a sentence
-# for example, if your two string values are 'Belle' and 'run', a sentence could be: See Belle run. ('run' is not in a variable)
-# hint - you can combine string and variables with a plus sign (+)
+# hint - you can combine string variables with a plus sign (+)
 
 print('')
 print('')
-print('(3) -------------------------------------------------------------------')
-print('')
-print('')
-
-
-print('')
-print('')
-print('(3) -------------------------------------------------------------------')
+print('(4) -------------------------------------------------------------------')
 print('')
 print('')
 # create a variable call alphabet 
 # assign as its value the whole alphabet spelled out in a string (in order, without spaces)
-# print it out
+# print out how many lets in the alphabet
+# you can find out a string's length with the BUILT-IN FUNCTION len()
+# use the len() function like this len( thingYouWantToMeasure )
+
+
+print('')
+print('')
+print('(5) -------------------------------------------------------------------')
+print('')
+print('')
 
 
 print('')
