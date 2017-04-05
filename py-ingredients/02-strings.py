cc41d8232b885cbf84d72854f5fdd3a5ce32162a
KevMBrown <kevmbabc@gmail.com>
2017-04-04 21:22:03 -0400

    I did this dad or whatever you want to say.

--- py-ingredients/02-strings.py
+++ py-ingredients/02-strings.py
@@ -7,8 +7,8 @@ print('(1) -------------------------------------------------------------------')
 print('')
 print('')
 # print out a string that says 'Hello, world!'
-# hint: you create a string by using quotes, single 'like this!' or double "like this!" 
-
+# hint: you create a string by using quotes, single 'like this!' or double "like this!"
+print('Hello, Belle!')
 
 print('')
 print('')
@@ -16,42 +16,50 @@ print('(2) -------------------------------------------------------------------')
 print('')
 print('')
 # create a variable named "words" and assign a string value to it, then print it out
-
+words = 'Yo boi, taco boi'
+print(words)
 print('')
 print('')
 print('(3) -------------------------------------------------------------------')
 print('')
 print('')
-# create two variables called "noun" and "verb", 
+# create two variables called "noun" and "verb",
 # assign string values to each
 # combine the noun and verb variables and print out the combination in a sentence
 # hint - you can combine string variables with a plus sign (+)
-
+noun = 'dog'
+verb = 'lazy '
+print(verb+noun)
 print('')
 print('')
 print('(4) -------------------------------------------------------------------')
 print('')
 print('')
-# create a variable call alphabet 
+# create a variable call alphabet
 # assign as its value the whole alphabet spelled out in a string (in order, without spaces)
 # print out how many lets in the alphabet
 # you can find out a string's length with the BUILT-IN FUNCTION len()
 # use the len() function like this len( thingYouWantToMeasure )
-
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+print(len(alphabet))
 
 print('')
 print('')
 print('(5) -------------------------------------------------------------------')
 print('')
 print('')
-
-
-print('')
-print('')
-print('(4) -------------------------------------------------------------------')
-print('')
-print('')
 # print out the first letter of the alphabet
+# characters in strings are numbered starting at 0
+# return a character by naming its number in a bracket: ex. variableName[character number]
+print(alphabet[0])
+
+print('')
+print('')
+print('(6) -------------------------------------------------------------------')
+print('')
+print('')
+# print out the f in the alphabet
+print(alphabet[5])
 
 print('')
 print('')
@@ -86,11 +94,3 @@ print('')
 print('(1) -------------------------------------------------------------------')
 print('')
 print('')
-
-
-print('')
-print('')
-print('(1) -------------------------------------------------------------------')
-print('')
-print('')
-
