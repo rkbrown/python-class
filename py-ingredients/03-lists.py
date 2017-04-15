f25578ee4180d20d709ba3358ff3c2beaf7d682a
Richard Brown <rbrown@rilm.org>
2017-04-15 13:25:33 -0400

    add lists test

---
+++ py-ingredients/03-lists.py
@@ -0,0 +1,84 @@
+print('***********************************************************************')
+print('******************************** LISTS ******************************')
+print('***********************************************************************')
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+# a LIST is a list of things
+# HINT: make a list of things like this:
+# friends = ['Gavin', 'Aidan', 'Jacob']
+# create the friends list above ( ^ ) and print it out
+friends
+
+
+print('')
+print('')
+print('(2) -------------------------------------------------------------------')
+print('')
+print('')
+# print out the first friend on the list
+# HINT: get a list item by specifying its OFFSET (its place number) EX: alphabet[18]
+
+
+print('')
+print('')
+print('(3) -------------------------------------------------------------------')
+print('')
+print('')
+# print out the second friend on the list
+
+
+print('')
+print('')
+print('(4) -------------------------------------------------------------------')
+print('')
+print('')
+# add a new friend to this friend list and print out the new list
+# HINT: add items to list with .append([thing you want to add]). EX: alphabet.append('z')
+
+
+print('')
+print('')
+print('(5) -------------------------------------------------------------------')
+print('')
+print('')
+# change the new friends name to 'Bob' print out the new list
+# HINT: your can change a list item by specifying its OFFSET and using the = sign to assign a new name
+
+
+
+print('')
+print('')
+print('(6) -------------------------------------------------------------------')
+print('')
+print('')
+# create a new list of kid's names and then add those kids to the friends list
+# print out the new list
+# HINT: you can combine two lists with .append() EX: alphabet.append(newStuff)
+# or, you can combine two lists with += EX: alphabet += newStuff
+
+
+print('')
+print('')
+print('(7) -------------------------------------------------------------------')
+print('')
+print('')
+# find out how many friends are on the list now, print it out
+# HINT: you can count items on a list by measuring its length using len() EX: alphabetNumber =len(alphabet)
+
+
+
+print('')
+print('')
+print('(8) -------------------------------------------------------------------')
+print('')
+print('')
+# remember STRINGS? remember you alphabet string from last lesson?
+# 1) re-create your alphabet STRING
+# 2) convert it to a LIST 
+# HINT: you can convert a string to a list with list() EX: catList = list('cat') creates ['c','a','t']
+# 3) print out the letter k
+# 4) print out the letter z
+# HINT: use negative OFFSET numbers to count backwards from the end of a list EX: catList[-1] = 't'
