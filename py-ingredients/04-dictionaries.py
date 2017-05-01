9e81cbe962f6f65c2e6b9e9d38aa37cee7c4d675
Richard Brown <rbrown@rilm.org>
2017-04-30 22:14:27 -0400

    add dictionary

---
+++ py-ingredients/04-dictionaries.py
@@ -0,0 +1,64 @@
+print('***********************************************************************')
+print('******************************** DICTIONARIES *************************')
+print('***********************************************************************')
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+# We want to make a list of friends that includes their real name and their XBox screen name
+# EX. friend's name = Aidan, screen name = 'MintPython'
+# A Python DICTIONARY is the way to go!!!
+# Here's an example of a DICTIONARY:
+ingredient = {
+    "name": "milk",
+    "amount": "1 cup",
+    }
+# Now create a dictionary that records the name of a kid along with his screen name.
+
+print('')
+print('')
+print('(2) -------------------------------------------------------------------')
+print('')
+print('')
+# That kid doesn't like his screen name. Please change it.
+# Here's how we increase the amount of milk.
+ingredient['amount'] = "1.5 cup"
+
+print('')
+print('')
+print('(3) -------------------------------------------------------------------')
+print('')
+print('')
+# What else do we know about the kid? Add that to his dictionary.
+# Here's how you add to the ingredient dictionary.
+ingredient['type'] = "liquid"
+
+
+print('')
+print('')
+print('(4) -------------------------------------------------------------------')
+print('')
+print('')
+# The kid is on a sports team. Make a new dictionary that tells about his sports team.
+# 
+
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+
+
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
+
+
+print('')
+print('')
+print('(1) -------------------------------------------------------------------')
+print('')
+print('')
