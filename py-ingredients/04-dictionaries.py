3e9bbc851b2671516eea43e84cc74dfe7cba4979
Kevin Brown <kevmbabc@gmail.com>
2017-05-13 12:25:00 -0400

    TACO

--- py-ingredients/04-dictionaries.py
+++ py-ingredients/04-dictionaries.py
@@ -7,7 +7,7 @@ print('(1) -------------------------------------------------------------------')
 print('')
 print('')
 # We want to make a list of friends that includes their real name and their XBox screen name
-# EX. friend's name = Aidan, screen name = 'MintPython'
+# EX. friend's name = Aidan, screen name = 'MintPython60169'
 # A Python DICTIONARY is the way to go!!!
 # Here's an example of a DICTIONARY:
 ingredient = {
@@ -15,7 +15,10 @@ ingredient = {
     "amount": "1 cup",
     }
 # Now create a dictionary that records the name of a kid along with his screen name.
-
+kid = {
+    "name": "Gavin",
+    "Screenname": "Gavdawg1014",
+    }
 print('')
 print('')
 print('(2) -------------------------------------------------------------------')
@@ -24,7 +27,7 @@ print('')
 # That kid doesn't like his screen name. Please change it.
 # Here's how we increase the amount of milk.
 ingredient['amount'] = "1.5 cup"
-
+kid['Screenname'] = "GavinDaT0co"
 print('')
 print('')
 print('(3) -------------------------------------------------------------------')
@@ -33,7 +36,7 @@ print('')
 # What else do we know about the kid? Add that to his dictionary.
 # Here's how you add to the ingredient dictionary.
 ingredient['type'] = "liquid"
-
+kid['Baseballposition'] = "CenterField"
 
 print('')
 print('')
@@ -41,13 +44,16 @@ print('(4) -------------------------------------------------------------------')
 print('')
 print('')
 # The kid is on a sports team. Make a new dictionary that tells about his sports team.
-# 
+team = {
+    "type": "baseball",
+    "team": "Cardinals",
+    }
 
 print('')
 print('')
 print('(1) -------------------------------------------------------------------')
-print('')
-print('')
+print(kid)
+print(team)
 
 
 print('')
